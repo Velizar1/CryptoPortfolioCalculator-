796c31ce0e9d040ff530da178c73b3de5c22e35a
v.d.vrubchev <v.d.vrubchev@gmail.com>
2025-09-04 22:50:12 +0300

    enable swagger in production

--- CryptoPortfolio/Program.cs
+++ CryptoPortfolio/Program.cs
@@ -84,15 +84,12 @@ namespace CryptoPortfolio
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crypto API v1");
-                    c.RoutePrefix = "swagger";
-                });
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Crypto API v1");
+                c.RoutePrefix = "swagger";
+            });
 
             app.UseRouting();
             app.UseHttpsRedirection();
